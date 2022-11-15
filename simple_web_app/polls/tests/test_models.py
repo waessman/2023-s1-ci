efb23978e729cab4385022ce494521fdeab9352d
André Portela <portela.eng@gmail.com>
2022-11-15 14:04:36 -0300

    poll app - testing module refactored

--- simple_web_app/polls/tests/test_models.py
+++ simple_web_app/polls/tests/test_models.py
@@ -1,7 +1,7 @@
 import datetime
 from django.test import TestCase
 from django.utils import timezone
-from .models import Question
+from ..models import Question
 
 
 class QuestionModelTests(TestCase):
