ab8f20a504a103faa12bf5dddb7e65314ced313a
André Portela <portela.eng@gmail.com>
2022-11-17 20:34:59 -0300

    DigitValidator 1 digit and 1 letter test

--- simple_web_app/tests/test_digit_validator.py
+++ simple_web_app/tests/test_digit_validator.py
@@ -3,3 +3,7 @@ from simple_web_app.string_validators import DigitValidator
 
 def test_1_digit_str_should_validate():
     assert None == DigitValidator(content="1").validate()
+
+
+def test_1_digit_and_1_letter_str_should_validate():
+    assert None == DigitValidator(content="1a").validate()
