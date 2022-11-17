5e42290fadfeada81d5abcfae5356c51b85309c2
André Portela <portela.eng@gmail.com>
2022-11-17 20:40:13 -0300

    DigitValidator invalid tests

--- simple_web_app/tests/test_digit_validator.py
+++ simple_web_app/tests/test_digit_validator.py
@@ -1,4 +1,6 @@
+import pytest
 from simple_web_app.string_validators import DigitValidator
+from simple_web_app.exceptions import NoDigitException
 
 
 def test_1_digit_str_should_validate():
@@ -7,3 +9,22 @@ def test_1_digit_str_should_validate():
 
 def test_1_digit_and_1_letter_str_should_validate():
     assert None == DigitValidator(content="1a").validate()
+
+
+def test_1_letter_and_1_digit_str_should_validate():
+    assert None == DigitValidator(content="b1").validate()
+
+
+def test_1_letter_str_should_raise_http_400_exception():
+    with pytest.raises(NoDigitException):
+        DigitValidator(content="a").validate()
+
+
+def test_1_especial_char_str_should_raise_http_400_exception():
+    with pytest.raises(NoDigitException):
+        DigitValidator(content="@").validate()
+
+
+def test_empty_str_should_raise_http_400_exception():
+    with pytest.raises(NoDigitException):
+        DigitValidator(content="").validate()
