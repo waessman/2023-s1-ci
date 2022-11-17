e86e425e589d4a4ed9c965301711a5aef4b77b9e
André Portela <portela.eng@gmail.com>
2022-11-17 15:16:56 -0300

    cleaning the test

--- simple_web_app/test_main.py
+++ simple_web_app/test_main.py
@@ -1,10 +1,12 @@
 from fastapi.testclient import TestClient
 from main import app
 
-
+# given
 client = TestClient(app)
 
 
 def test_read_main_status():
+    # when
     response = client.get("/")
+    # then
     assert response.status_code == 200
