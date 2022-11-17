516be5d4ffee9eb6041e1f2736dabc49eb432263
André Portela <portela.eng@gmail.com>
2022-11-17 15:27:02 -0300

    added a test for misspelled key in password validation body

--- simple_web_app/test_main.py
+++ simple_web_app/test_main.py
@@ -5,7 +5,7 @@ from main import app
 client = TestClient(app)
 
 
-def test_read_main_status():
+def test_read_main_should_return_status_200():
     # when
     response = client.get("/")
     # then
@@ -17,3 +17,12 @@ def test_read_main_response():
     response = client.get("/")
     # then
     assert response.json() == {"description": "Please submit a post with a password for validation on validate_password view"}
+
+
+def test_password_validation_misspelled_key_password_should_return_status_422():
+    # given
+    misspelled_key_password_dict = {"passwrd": ""}
+    # when
+    response = client.post("/", data=misspelled_key_password_dict)
+    # then
+    assert response.status_code == 422
