5727a80a9977f0629eb4f9869927e7de42c68322
André Portela <portela.eng@gmail.com>
2022-11-18 14:59:29 -0300

    refactoring Exceptions

--- simple_web_app/exceptions.py
+++ simple_web_app/exceptions.py
@@ -1,11 +1,18 @@
 from fastapi import HTTPException
 
 
-class MinimumLengthException(HTTPException):
+class InvalidPasswordException(HTTPException):
     def __init__(self, detail: str):
         super().__init__(status_code=400, detail=detail)
 
 
-class NoDigitException(HTTPException):
-    def __init__(self, detail: str):
-        super().__init__(status_code=400, detail=detail)
+class MinimumLengthException(InvalidPasswordException):
+    pass
+
+
+class NoDigitException(InvalidPasswordException):
+    pass
+
+
+class NoLowerCaseException(InvalidPasswordException):
+    pass
